97a19fb234e45096b630ca5c6d8e3a04e92d17ab
Luciano <61517478+devluciano@users.noreply.github.com>
2025-03-12 11:14:17 -0300

    código
    códigos com problemas retirados.

--- Models/ResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CrudDAPPERApi.Models
-{
-    public class ResponseModel<T>
-    {
-        public T? Dados { get; set; }
-        public string Mensagem { get; set; } = string.Empty;
-        public bool  Status { get; set; } = true;
-    }
-}
