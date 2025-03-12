00cd75963b2907d5f3ac2b8242213fdd5ee5100b
Luciano <61517478+devluciano@users.noreply.github.com>
2025-03-12 11:05:18 -0300

    implemento
    subindo primeiros códigos

---
+++ Services/UsuarioInteface.cs
@@ -0,0 +1,11 @@
+﻿using CrudDAPPERApi.Dto;
+using CrudDAPPERApi.Models;
+
+namespace CrudDAPPERApi.Services
+{
+    public interface UsuarioInteface
+    {
+        Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios();
+        Task<ResponseModel<UsuarioListarDto>> BuscarUsuarioPorId(int UsuarioId);
+    }
+}
---
+++ Services/UsuarioService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using CrudDAPPERApi.Dto;
+using CrudDAPPERApi.Models;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace CrudDAPPERApi.Services
+{
+    public class UsuarioService : UsuarioInteface
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+        public UsuarioService(IConfiguration configuration, IMapper mapper)
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseModel<UsuarioListarDto>> BuscarUsuarioPorId(int UsuarioId)
+        {
+            ResponseModel<UsuarioListarDto> response = new ResponseModel<UsuarioListarDto>();
+
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnetion")))
+            {
+                var usuarioBanco = await connection.QueryFirstOrDefaultAsync<Usuario>("select * from Usuarios where Id = @Id", new {Id = UsuarioId});
+
+                if (usuarioBanco == null)
+                {
+                    response.Mensagem = "Nenhum usuário localizado!";
+                    response.Status = false;
+                    return response;
+                }
+
+                var usuarioMapeado = _mapper.Map<UsuarioListarDto>(usuarioBanco);
+
+                response.Dados = usuarioMapeado;
+                response.Mensagem = "Usuário localizado com sucesso!";
+                
+            }
+
+            return response;
+        }
+
+        public async Task<ResponseModel<List<UsuarioListarDto>>> BuscarUsuarios()
+        {
+
+            ResponseModel<List<UsuarioListarDto>> response = new ResponseModel<List<UsuarioListarDto>>();
+
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnetion")))
+            {
+                var usuariosBanco = await connection.QueryAsync<Usuario>("select * from Usuarios");
+
+                if (usuariosBanco.Count() == 0)
+                {
+                    response.Mensagem = "Nenhum usuário localizado!";
+                    response.Status = false;
+                    return response;
+                }
+                
+                //Transformação Mapper
+
+                var usuarioMapeado = _mapper.Map<List<UsuarioListarDto>>(usuariosBanco);
+
+                response.Dados = usuarioMapeado;
+                response.Mensagem = "Usuários Localizados com sucesso!";
+
+             
+            }
+
+            return response;
+        }
+    }
+}
